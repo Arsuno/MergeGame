7524c3fbbd0153c7528776a0ca89b23b509738a6
Arsuno <Arsuno2002@gmail.com>
2021-10-23 10:41:02 +0500

    MergeGame

---
+++ MergeColorGame/Assets/Scripts/Cube.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CubeColor
+{
+    Red,
+    Green,
+    Blue,
+    Yellow
+}
+
+public class Cube : MonoBehaviour
+{
+    public CubeColor _cubeColor;
+
+    [SerializeField] private Material _objectMaterial;
+
+    public void Initialize(CubeColor cubeColor)
+    {
+        _cubeColor = cubeColor;
+        SetMaterialColor(_cubeColor);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Cube cube) == true)
+        {
+            if (_objectMaterial.color == cube._objectMaterial.color)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private uint SetMaterialColor(CubeColor color)
+    {
+        switch (color)
+        {
+            case CubeColor.Red: _objectMaterial.color = Color.red; break;
+            case CubeColor.Green: _objectMaterial.color = Color.green; break;
+            case CubeColor.Blue: _objectMaterial.color = Color.blue; break;
+            case CubeColor.Yellow: _objectMaterial.color = Color.yellow; break;
+        }
+        return 0;
+    }
+
+
+
+
+
+
+}
---
+++ MergeColorGame/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField] private Cube _cubePrefab;
+    [SerializeField] private GameObject _spawnPoint;
+    private Cube _spawnedCube;
+    private int GeneratedColor;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            _cubePrefab.Initialize(GenerateRandomColor());
+            _spawnedCube = Instantiate(_cubePrefab, _spawnPoint.transform.position, Quaternion.identity); 
+        }
+    }
+
+    private CubeColor GenerateRandomColor()
+    {
+        Array colors = Enum.GetValues(typeof(CubeColor));
+        return (CubeColor)colors.GetValue(UnityEngine.Random.Range(0, colors.Length));
+    }
+
+}
